4b9e02fd552e332404aed443a38d8dccdedb4d66
Jasi <normalc@posteo.net>
2025-06-23 19:29:58 -0400

    asahi-audio: create package meta attributes

--- apple-silicon-support/packages/asahi-audio/default.nix
+++ apple-silicon-support/packages/asahi-audio/default.nix
@@ -39,4 +39,38 @@ stdenvNoCC.mkDerivation (finalAttrs: {
       triforce-lv2
     ];
   };
+
+  meta = {
+    description = "Linux userspace audio configuration for Apple Silicon Macs";
+    longDescription = ''
+      This package contains DSP configuration files for Apple Silicon
+      Macs supported by the Asahi Linux project. The goal is to make
+      the Asahi Linux audio experience better than macOS, and in doing
+      so demonstrate that desktop Linux audio can be made fit for
+      purpose with a little bit of effort.
+
+      Currently Supported Devices (speakers)
+        - MacBook Air (13-inch, M1, 2020)
+        - MacBook Air (13-inch, M2, 2022)
+        - MacBook Air (15-inch, M2, 2023)
+        - MacBook Pro (13-inch, M1/M2, 2020/2022)
+        - MacBook Pro (14-inch, M1/M2 Pro/Max, 2021/2023)
+        - MacBook Pro (16-inch, M1/M2 Pro/Max, 2021/2023)
+        - Mac mini (M1/M2/M2 Pro, 2020/2023)
+        - Mac Studio (M1/M2 Max/Ultra, 2022/2023)
+
+      Currently Supported Devices (microphones)
+        - MacBook Air (13-inch, M1, 2020)
+        - MacBook Air (13-inch, M2, 2022)
+        - MacBook Air (15-inch, M2, 2023)
+        - MacBook Pro (13-inch, M1/M2, 2020/2022)
+        - MacBook Pro (14-inch, M1/M2 Pro/Max, 2021/2023)
+        - MacBook Pro (16-inch, M1/M2 Pro/Max, 2021/2023)
+    '';
+    homepage = "https://github.com/AsahiLinux/asahi-audio";
+    changelog = "https://github.com/AsahiLinux/asahi-audio/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ normalcea ];
+    platforms = lib.platforms.linux;
+  };
 })
