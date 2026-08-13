61ce5abc4ac1416062af2b5c554568782a1cc013
Eli Saado <eli5saado@gmail.com>
2026-08-13 17:27:19 +0200

    linux-asahi: 7.1.6-1 -> 7.1.8-1

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -26,15 +26,15 @@ let
         inherit stdenv lib;
 
         pname = "linux-asahi";
-        version = "7.1.5";
+        version = "7.1.8";
         modDirVersion = version;
         extraMeta.branch = "7.1";
 
         src = fetchFromGitHub {
           owner = "AsahiLinux";
           repo = "linux";
-          tag = "asahi-7.1.5-2";
-          hash = "sha256-z7S0YTmDshMK2frFhMm4M4wUOV3rPOwxPkR2IXk4R+Y=";
+          tag = "asahi-7.1.8-1";
+          hash = "sha256-hsRB5FJz3hRR0QakBY7+dRXD5p9z6ZQiuJZgghpaTjY=";
         };
 
         kernelPatches = [
